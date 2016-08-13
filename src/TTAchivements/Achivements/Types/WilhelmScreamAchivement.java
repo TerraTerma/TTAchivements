7d4d39756b6fd942ce2411ec8c41504ae5bc52ac
mosemister <nekronmoseley@gmail.com>
2016-08-13 15:32:12 +0100

    fixed WilhelmScream

--- src/TTAchivements/Achivements/Types/WilhelmScreamAchivement.java
+++ src/TTAchivements/Achivements/Types/WilhelmScreamAchivement.java
@@ -5,21 +5,23 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
 
 import TTAchivements.Achivements.Achivement;
 import TTAchivements.Achivements.Reward;
 import TTCore.Entity.Living.Human.Player.TTPlayer;
 
 public class WilhelmScreamAchivement implements Achivement {
-	
+
 	List<AttackData> ATTACKDATA = new ArrayList<>();
-	
+
 	@Override
 	public String getName() {
 		return "*Wilhelm Screams*";
@@ -39,48 +41,55 @@ public class WilhelmScreamAchivement implements Achivement {
 	public boolean isRepeatable() {
 		return true;
 	}
-	
+
 	@EventHandler
-	public void playerHitEvent(EntityDamageByEntityEvent event){
-		if(event.getDamager() instanceof Player){
-			TTPlayer player = TTPlayer.getPlayer((Player)event.getDamager());
+	public void playerHitEvent(EntityDamageByEntityEvent event) {
+		if (event.getDamager() instanceof Player) {
+			TTPlayer player = TTPlayer.getPlayer((Player) event.getDamager());
 			ATTACKDATA.add(new AttackData(event.getEntity(), player));
 		}
 	}
-	
+
 	@EventHandler
-	public void playerHealEvent(EntityRegainHealthEvent event){
+	public void playerHealEvent(EntityRegainHealthEvent event) {
 		Optional<AttackData> opAttack = ATTACKDATA.stream().filter(a -> a.getEntity().equals(event.getEntity())).findFirst();
-		if(opAttack.isPresent()){
+		if (opAttack.isPresent()) {
 			ATTACKDATA.remove(opAttack.get());
 		}
 	}
-	
+
 	@EventHandler
-	public void playerDeathEvent(PlayerDeathEvent event){
-		Optional<AttackData> opAttack = ATTACKDATA.stream().filter(a -> a.getEntity().equals(event.getEntity())).findFirst();
-		if(opAttack.isPresent()){
-			AttackData data = opAttack.get();
-			ATTACKDATA.remove(data);
-			Achivement.unlockAchivement(data.getAttacker(), this);
+	public void playerDeathEvent(EntityDamageEvent event) {
+		if (event.getCause().equals(DamageCause.FALL)) {
+			if (event.getEntity() instanceof Damageable) {
+				Damageable entity = (Damageable) event.getEntity();
+				if ((entity.getHealth() - event.getDamage()) <= 0) {
+					Optional<AttackData> opAttack = ATTACKDATA.stream().filter(a -> a.getEntity().equals(entity)).findFirst();
+					if (opAttack.isPresent()) {
+						AttackData data = opAttack.get();
+						ATTACKDATA.remove(data);
+						Achivement.unlockAchivement(data.getAttacker(), this);
+					}
+				}
+			}
 		}
 	}
-	
-	public static class AttackData{
-		
+
+	public static class AttackData {
+
 		Entity ENTITY;
 		TTPlayer ATTACKER;
-		
-		public AttackData(Entity entity, TTPlayer attacker){
+
+		public AttackData(Entity entity, TTPlayer attacker) {
 			ENTITY = entity;
 			ATTACKER = attacker;
 		}
-		
-		public Entity getEntity(){
+
+		public Entity getEntity() {
 			return ENTITY;
 		}
-		
-		public TTPlayer getAttacker(){
+
+		public TTPlayer getAttacker() {
 			return ATTACKER;
 		}
 	}
--- src/TTAchivements/Mech/AchivementData.java
+++ src/TTAchivements/Mech/AchivementData.java
@@ -46,7 +46,7 @@ public class AchivementData implements SavableData, PlayerData {
 				}
 			});
 		}
-		return false;
+		return true;
 	}
 
 }
